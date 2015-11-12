d3e47cc605b526c0bab92f8e5275741e39a6d9ee
Ramon Casamajó <rcasamajo@iespoblenou.org>
2015-11-12 10:44:50 +0100

    inici

--- src/eltempsdsktp/Controller.java
+++ src/eltempsdsktp/Controller.java
@@ -1,4 +1,13 @@
 package eltempsdsktp;
 
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+    public ListView lvLlistaPrediccions;
+    public ImageView ivIconPrediccio;
+
+    void initialize() {
+
+    }
 }
