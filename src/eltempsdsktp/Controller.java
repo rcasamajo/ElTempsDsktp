6808e0399013c1334dd07a559b4d6e876f53d6da
rcasamajo <rcasamajo@iespoblenou.org>
2015-11-19 16:04:12 +0100

    dialog

--- src/eltempsdsktp/Controller.java
+++ src/eltempsdsktp/Controller.java
@@ -24,16 +24,22 @@ public class Controller {
         dialog.setHeaderText("This is a custom dialog");
         dialog.setResizable(true);
 
+        // Afegim controls i els organitzem en un GridPane
         Label label1 = new Label("Predicció: ");
         ImageView img1 = new ImageView("01d.png");
         Label label2 = new Label("Sol");
+        // No cal definir el tamany deñ GridPane al crear-lo
         GridPane grid = new GridPane();
-        grid.add(label1, 1, 1);
+        grid.add(label1, 0, 0);
         grid.add(img1, 1, 2);
         grid.add(label2, 2, 2);
 
+        // Afegim el Grid al DialogPane que conté Dialog.
+        // DialogPane és bàsicament una plantilla on organitzar el contingut del diàleg
         dialog.getDialogPane().setContent(grid);
 
+        // Afegim un botó del tipus predefinit a ButtonData OK_DONE
+        // L'afegim a la llista de botons del diàleg (getButtonTypes()) de manera que es situa de manera estàndard
         ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
         dialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
     }
